62fcd259bbf4391f562f41d2ab4e583b0318a04e
Daniel Yung-Tse Hsiao <e5q8@ugrad.cs.ubc.ca>
2015-05-24 01:17:47 -0700

    added operator scripts

---
+++ Assets/Scripts/MinusHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinusHandler : MonoBehaviour{
+    string op;
+    // Use this for initialization
+    void Start()
+    {
+        op = "-"
+        this.transform.position = Vector3.up * 2.0f * Random.Range(-1.0f, 1.0f) + Vector3.right * 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.position -= Vector3.right * 3.0f * Time.deltaTime;
+        if(this.transform.position.x <= -32.0f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        string op = PlayerPrefs.GetString("op");
+        if(op == null)
+        {
+            switch (op)
+            {
+            PlayerPrefs.SetString("op", null);
+        } else
+        {
+            //Load End Game Screen;
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlusHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlusHandler : MonoBehaviour{
+    string op;
+    // Use this for initialization
+    void Start()
+    {
+        op = "+";
+        this.transform.position = Vector3.up * 2.0f * Random.Range(-1.0f, 1.0f) + Vector3.right * 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.position -= Vector3.right * 3.0f * Time.deltaTime;
+        if(this.transform.position.x <= -32.0f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        string op = PlayerPrefs.GetString("op");
+        if(op == null)
+        {
+            PlayerPrefs.SetString("op", "+");
+        } else
+        {
+            //Load End Game Screen;
+        }
+        Destroy(this.gameObject);
+    }
+}
