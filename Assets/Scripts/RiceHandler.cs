9b70425134c83e5034f387999615f047e1fb0cd7
Daniel Yung-Tse Hsiao <e5q8@ugrad.cs.ubc.ca>
2015-05-23 20:17:39 -0700

    adds spawn rice

--- Assets/Scripts/RiceHandler.cs
+++ Assets/Scripts/RiceHandler.cs
@@ -6,12 +6,12 @@ public class RiceHandler : MonoBehaviour{
     // Use this for initialization
     void Start()
     {
-        this.transform.position = Vector3.up * 2.0f * Random.Range(-1.0f, 1.0f);
+        this.transform.position = Vector3.up * 2.0f * Random.Range(-1.0f, 1.0f) + Vector3.right * 10.0f;
     }
 
     // Update is called once per frame
     void Update () {
-        this.transform.position -= Vector3.right * 7.0f * Time.deltaTime;
+        this.transform.position -= Vector3.right * 3.0f * Time.deltaTime;
         if(this.transform.position.x <= -32.0f)
         {
             Destroy(this.gameObject);
--- Assets/Scripts/SpawnRiceHandler.cs
+++ Assets/Scripts/SpawnRiceHandler.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SpawnRiceHandler : MonoBehaviour {
     float spawnTimer = 0.0f;
     public float timeBetweenRice = 1.5f;
+    public GameObject rice;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,5 +13,10 @@ public class SpawnRiceHandler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         spawnTimer -= Time.deltaTime;
+        if(spawnTimer < 0.0f)
+        {
+            spawnTimer = timeBetweenRice;
+            Instantiate(rice, Vector3.right * 20f, Quaternion.identity);
+        }
 	}
 }
