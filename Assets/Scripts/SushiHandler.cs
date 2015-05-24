604d60ac8f789c8cfd45eb02f4a3e0dc1c6bc891
puggie-wuggie <yoola93@gmail.com>
2015-05-23 18:08:19 -0700

    first commit

---
+++ Assets/Scripts/SushiHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SushiHandler : MonoBehaviour {
+    Rigidbody2D _Rigidbody2D;
+    // Use this for initialization
+    void Start()
+    {
+        _Rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+    
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+           _Rigidbody2D.velocity = Vector2.zero;
+            _Rigidbody2D.AddForce(Vector2.up* 10.0f, ForceMode2D.Impulse);
+        }
+
+        _Rigidbody2D.rotation = _Rigidbody2D.velocity.y * 2.0f;
+    }
+}
