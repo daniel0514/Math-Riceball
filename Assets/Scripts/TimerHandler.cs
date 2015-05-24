ab2e44b1b5fb4899fa02115e0e1052e635899844
Jonh <john9000100@gmail.com>
2015-05-24 12:03:40 -0700

    added timer

---
+++ Assets/Scripts/TimerHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerHandler : MonoBehaviour
+{
+
+    static float timer = 60.00f;
+    public Text text_box;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        text_box.text = timer.ToString("0");
+
+        if (timer == 0)
+        {
+            //endGame();
+        }
+    }
+
+}
+
