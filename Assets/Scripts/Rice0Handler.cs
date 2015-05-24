b39f87a0205baebf4efebd41263df8fbbc54f396
Daniel Yung-Tse Hsiao <e5q8@ugrad.cs.ubc.ca>
2015-05-24 00:27:03 -0700

    added scripts for value rice

---
+++ Assets/Scripts/Rice0Handler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rice0Handler : MonoBehaviour{
+    int value;
+    // Use this for initialization
+    void Start()
+    {
+        value = 0;
+        this.transform.position = Vector3.up * 2.0f * Random.Range(-1.0f, 1.0f) + Vector3.right * 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.position -= Vector3.right * 3.0f * Time.deltaTime;
+        if(this.transform.position.x <= -32.0f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        int result = PlayerPrefs.GetInt("result");
+        string op = PlayerPrefs.GetString("op");
+        if(op != null)
+        {
+            switch (op)
+            {
+                case "+":
+                    result += value;
+                    break;
+                case "-":
+                    result -= value;
+                    break;
+                case "*":
+                    result *= value;
+                    break;
+                case "/":
+                    result /= value;
+                    break;
+                default:
+                    break;
+            }
+            PlayerPrefs.SetInt("result", result);
+            PlayerPrefs.SetString("op", null);
+        } else
+        {
+            //Load End Game Screen;
+        }
+        Destroy(this.gameObject);
+    }
+}
