be7438ff15a62ca1b78b1252287881dfb4a318da
Daniel Yung-Tse Hsiao <e5q8@ugrad.cs.ubc.ca>
2015-05-23 20:00:41 -0700

    added Rice and working on SpawnRice

---
+++ Assets/Scripts/RiceHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiceHandler : MonoBehaviour{
+    
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = Vector3.up * 2.0f * Random.Range(-1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.position -= Vector3.right * 7.0f * Time.deltaTime;
+        if(this.transform.position.x <= -32.0f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/SpawnRiceHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnRiceHandler : MonoBehaviour {
+    float spawnTimer = 0.0f;
+    public float timeBetweenRice = 1.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTimer -= Time.deltaTime;
+	}
+}
