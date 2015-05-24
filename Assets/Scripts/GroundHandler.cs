f3ef00bf9a20af3c6b1da3702a2160f44778f091
puggie-wuggie <yoola93@gmail.com>
2015-05-23 19:27:44 -0700

    new permanent background

---
+++ Assets/Scripts/GroundHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //move ground 3 units per second
+        this.transform.position -= Vector3.right * 3.0f * Time.deltaTime;
+
+        //ground looping
+        if(this.transform.position.x <= -39.95f)
+        {
+            this.transform.position = Vector3.right * 39.95f;
+        }
+	}
+}
