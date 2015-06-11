1b462c781f487a43b6a0a84c2f6814d66a65d65c
Jonh <john9000100@gmail.com>
2015-06-10 23:44:24 -0700

    show current op/num

---
+++ Assets/Scripts/CurrentNumber.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrentNumber : MonoBehaviour {
+
+    SpriteRenderer number;
+    public Sprite[] numbers;
+	// Use this for initialization
+	void Start () {
+        number = this.GetComponent<SpriteRenderer>();
+        //numbers = new Sprite[10];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int last_num = ResultAndOpsHandler.last_num;
+        if (last_num == -1)
+            number.sprite = null;
+        else
+            number.sprite = numbers[last_num];
+    }
+}
