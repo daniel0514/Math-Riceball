da4b61ab1c93e45b693e40bab9f478bda8916ac9
Daniel Yung-Tse Hsiao <e5q8@ugrad.cs.ubc.ca>
2015-06-13 01:24:20 -0700

    added equation display

---
+++ Assets/Scripts/EquationHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EquationHandler : MonoBehaviour {
+    bool showEquation = false;
+    Text equationText;
+	// Use this for initialization
+	void Start () {
+        equationText = GetComponent<Text>();
+	    if(showEquation == false)
+        {
+            equationText.enabled = false;
+        } else
+        {
+            equationText.enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        equationText.text = ResultAndOpsHandler.equation;
+	}
+}
--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -21,11 +21,12 @@ public class ScoreHandler : MonoBehaviour {
             {
                 Debug.Log("Beat HighestScore");
                 Debug.Log("Cur: " + curScore + " high" + highestScore);
-                scoreText.text = scoreText.text + "\nAnd You Beat Your \nHighest Score!\nHighest Score: " + highestScore;
+                scoreText.text = scoreText.text + "Highest Score: " + highestScore +  "\n\n Almost Beat Your Highest Score!";
             } else
             {
                 Debug.Log("Did not beat");
                 Debug.Log("Cur: " + curScore + " high" + highestScore);
+                scoreText.text = scoreText.text + "\nYou Almost Beat Your \nHighest Score!\nHighest Score: " + highestScore;
             }
         }
     }
